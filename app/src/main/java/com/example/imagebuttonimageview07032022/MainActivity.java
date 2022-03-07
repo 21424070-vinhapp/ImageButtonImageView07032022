557306f256b537990768b6abf5e6d7dc12992335
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-03-07 16:44:31 +0700

    vi du ve ImageButton ImageView

--- app/src/main/java/com/example/imagebuttonimageview07032022/MainActivity.java
+++ app/src/main/java/com/example/imagebuttonimageview07032022/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.imagebuttonimageview07032022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button mBtnDoiHinh;
+    ImageButton mBtnClose;
+    ImageView mImgHinh, mImgClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnClose=findViewById(R.id.btnClose);
+        mBtnDoiHinh=findViewById(R.id.btnDoiHinh);
+        mImgHinh=findViewById(R.id.imgHinh);
+        mImgClose=findViewById(R.id.imgClose);
+
+        mBtnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mBtnDoiHinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mImgHinh.getTag()==null||mImgHinh.getTag().equals("h1"))
+                {
+                    mImgHinh.setImageResource(R.drawable.bohoahong);
+                    mImgHinh.setTag("h2");
+                }
+                else
+                {
+                    mImgHinh.setImageResource(R.drawable.hoahongdo);
+                    mImgHinh.setTag("h1");
+                }
+            }
+        });
     }
 }
